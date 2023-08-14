eb4f6747fa166653b07dc1a72ed5217538b5102c
Nathan Baldez <nathanbsb2@hotmail.com>
2023-08-14 18:26:40 -0300

    feat: create User repository

---
+++ src/main/java/com/fintechsimplificada/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.fintechsimplificada.repositories;
+
+import com.fintechsimplificada.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserByDocument(Long id);
+}
