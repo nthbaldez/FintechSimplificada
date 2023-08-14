af6805dff2db614a238aac510f34fa7e41ad91f9
Nathan Baldez <nathanbsb2@hotmail.com>
2023-08-14 18:25:58 -0300

    feat: initial config

---
+++ src/main/java/com/fintechsimplificada/FintechsimplificadaApplication.java
@@ -0,0 +1,13 @@
+package com.fintechsimplificada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechsimplificadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechsimplificadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintechsimplificada/FintechsimplificadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintechsimplificada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechsimplificadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
