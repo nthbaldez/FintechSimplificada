32c00b9e626c4903febeffeb84bea6d1aa01fda9
Nathan Baldez <nathanbsb2@hotmail.com>
2023-08-14 22:55:30 -0300

    feat: create Controllers

---
+++ src/main/java/com/fintechsimplificada/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.fintechsimplificada.controllers;
+
+import com.fintechsimplificada.domain.user.User;
+import com.fintechsimplificada.dtos.UserDTO;
+import com.fintechsimplificada.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO user){
+        System.out.println(user);
+        User newUser = userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> allUsers = this.userService.getAllUsers();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+}
