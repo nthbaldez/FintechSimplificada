5a15f1e39e67e387fd0cb6375e2f6b629c57114d
Nathan Baldez <nathanbsb2@hotmail.com>
2023-08-14 22:55:58 -0300

    feat: create Services

---
+++ src/main/java/com/fintechsimplificada/services/UserService.java
@@ -0,0 +1,45 @@
+package com.fintechsimplificada.services;
+
+import com.fintechsimplificada.domain.user.User;
+import com.fintechsimplificada.domain.user.UserType;
+import com.fintechsimplificada.dtos.UserDTO;
+import com.fintechsimplificada.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo Lojista não está autorizado a realizar este tipo de transacação.");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+
+    public User createUser(UserDTO userData) {
+        User newUser = new User(userData);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
+    }
+
+}
